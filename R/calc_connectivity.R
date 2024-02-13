f30769c4a9884deb7a390923f95d556d6f6800b3
Sz-Tim <tim.szewczyk@gmail.com>
2024-02-13 14:56:45 +0000

    lumping functions into sensible scripts

---
+++ R/calc_connectivity.R
@@ -0,0 +1,83 @@
+#' Load pairwise connectivity matrices
+#'
+#' @param f Vector of filenames
+#' @param site_names Vector of site names; length(site_names) = ncol(connect_mx)
+#'
+#' @return Dataframe with a column for each site (destinations), and rows for each site (source) x date combination
+#' @export
+#'
+load_connectivity <- function(f, site_names) {
+  library(tidyverse)
+  f_base <- basename(f)
+  map_dfr(seq_along(f),
+          ~read_delim(f[.x], delim=" ", col_names=site_names, col_types="d") |>
+            mutate(source=factor(site_names, levels=site_names),
+                   date=ymd(str_split_fixed(f_base[.x], "_", 3)[,2])))
+}
+
+
+
+
+#' Calculate influx
+#'
+#' @param data Dataframe in long format
+#' @param dest_col Destination site (unquoted column name)
+#' @param N_col Lice density (unquoted column name)
+#' @param ... Grouping columns (unquoted)
+#'
+#' @return Summarised dataframe with column \code{total} giving the sum. Note
+#' that this includes self infection.
+#' @export
+#'
+calc_influx <- function(data, dest_col, N_col, ...) {
+  library(tidyverse)
+  data |>
+    group_by(dest_col, ...) |>
+    summarise(total=sum({{N_col}})) |>
+    ungroup()
+}
+
+
+
+
+#' Calculate self infection
+#'
+#' @param data Dataframe in long format
+#' @param src_col Source site (unquoted column name)
+#' @param dest_col Destination site (unquoted column name)
+#' @param N_col Lice density (unquoted column name)
+#' @param ... Grouping columns (unquoted)
+#'
+#' @return Summarised dataframe with column \code{total} giving the sum
+#' @export
+#'
+calc_self_infection <- function(data, src_col, dest_col, N_col, ...) {
+  library(tidyverse)
+  data |>
+    filter({{src_col}} == {{dest_col}}) |>
+    group_by(...) |>
+    summarise(total=sum({{N_col}})) |>
+    ungroup()
+}
+
+
+
+
+#' Calculate outflux
+#'
+#' @param data Dataframe in long format
+#' @param src_col Source site (unquoted column name)
+#' @param N_col Lice density (unquoted column name)
+#' @param ... Grouping columns (unquoted)
+#'
+#' @return Summarised dataframe with column \code{total} giving the sum. Note
+#' that this includes self infection.
+#' @export
+#'
+calc_outflux <- function(data, src_col, N_col, ...) {
+  library(tidyverse)
+  data |>
+    group_by(src_col, ...) |>
+    summarise(total=sum({{N_col}})) |>
+    ungroup()
+}

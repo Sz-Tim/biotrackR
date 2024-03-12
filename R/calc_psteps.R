f2dd17a73da75c3dfeb3295e8f2a0faff577abb0
Sz-Tim <tim.szewczyk@gmail.com>
2024-03-12 12:40:42 +0000

    function to calculate average particle densities by element

--- R/calc_psteps.R
+++ R/calc_psteps.R
@@ -56,7 +56,7 @@ load_psteps <- function(f, site_names=NULL) {
 #' @return Wide format dataframe
 #' @export
 #'
-load_psteps_simSets <- function(out_dir, mesh_i, sim_i, ncores=8,
+load_psteps_simSets <- function(out_dir, mesh_i, sim_i, ncores=4,
                                 stage="Mature", liceScale=28.2*240,
                                 per_m2=TRUE, log=TRUE) {
   library(tidyverse); library(glue); library(furrr)
@@ -109,7 +109,7 @@ load_psteps_simSets <- function(out_dir, mesh_i, sim_i, ncores=8,
 #'   \code{-9999}.
 #' @export
 #'
-calc_psteps_diff <- function(ps_wide, sims_comp, ncores) {
+calc_psteps_diff <- function(ps_wide, sims_comp, ncores=4) {
   library(tidyverse); library(furrr)
   plan(multisession, gc=T, workers=ncores)
 
@@ -133,3 +133,75 @@ calc_psteps_diff <- function(ps_wide, sims_comp, ncores) {
   return(psdiff_wide)
 }
 
+
+
+
+
+
+
+
+
+
+#' Calculate average particle densities
+#'
+#' @param ps_long Long-format dataframe
+#' @param y_col Quoted column name with particle densities
+#' @param grp_col \code{NULL} or quoted column name with time to average over (e.g., years or months)
+#' @param ncores Number of cores for parallelization with furrr
+#' @param mesh_sf \code{NULL} or mesh sf object
+#'
+#' @return
+#' @export
+#'
+#' @examples
+calc_psteps_avg <- function(ps_long, y_col, grp_col=NULL, ncores=4, mesh_sf=NULL) {
+  library(tidyverse); library(furrr); library(carrier)
+
+  crate_summary <- crate(~data.frame(pOcc=mean(!is.na(.x)),
+                                     mean_N=mean(.x, na.rm=T),
+                                     median_N=stats::median(.x, na.rm=T),
+                                     sd_N=stats::sd(.x, na.rm=T),
+                                     max_N=max(.x, na.rm=T)))
+  opts <- furrr_options(globals = FALSE)
+  plan(multisession, workers=ncores)
+
+  if(is.null(grp_col)) {
+    ps_avg <- ps_long |>
+      select("sim", "i", all_of(y_col)) |>
+      group_by(sim, i) |>
+      nest(data=.data[[y_col]]) |>
+      ungroup() |>
+      mutate(data=map(data, ~.x[[y_col]]),
+             summary=future_map(data, crate_summary, .options=opts)) |>
+      select(-data) |>
+      unnest(summary) |>
+      mutate(CV_N=sd_N/mean_N)
+  } else {
+    ps_avg <- ps_long |>
+      select("sim", "i", all_of(c(y_col, grp_col))) |>
+      group_by(sim, i, .data[[grp_col]]) |>
+      nest(data=.data[[y_col]]) |>
+      ungroup() |>
+      mutate(data=map(data, ~.x[[y_col]]),
+             summary=future_map(data, crate_summary, .options=opts)) |>
+      select(-data) |>
+      unnest(summary) |>
+      mutate(CV_N=sd_N/mean_N)
+  }
+
+  if(!is.null(mesh_sf)) {
+    ps_avg <- ps_avg |>
+      left_join(mesh_sf |> select(i, geom), y=_, by="i")
+  }
+
+  if(is.null(grp_col)) {
+    ps_avg <- ps_avg |>
+      filter(!is.na(sim)) # elements with no particles ever
+  } else {
+    ps_avg <- ps_avg |>
+      filter(!is.na(sim) & !is.na(.data[[grp_col]])) # elements with no particles ever
+  }
+
+  plan(sequential)
+  return(ps_avg)
+}

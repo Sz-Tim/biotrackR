d1696855be5ca3bf6856e2e77d2d223fe7626975
Sz-Tim <tim.szewczyk@gmail.com>
2024-04-02 16:02:31 +0100

    functions to download all data available from aquaculture.scot.gov.uk

--- R/download_data.R
+++ R/download_data.R
@@ -2,10 +2,14 @@
 #' Download weekly Scottish sea lice counts
 #'
 #' Accessed through aquaculture.scotland.gov.uk. Downloads a json file from the
-#' ArcGIS online map viewer, then processes that into a csv. Note that only data
-#' from March 2021 are available through this function. Older data must be
-#' downloaded manually from the website. Data are the weekly average number of
-#' adult female Lepeophtheirus salmonis reported by the salmon companies.
+#' ArcGIS online map viewer, then processes that into a csv.
+#'
+#' Data from before 2021-March-29 must be downloaded manually from the website.
+#' Data are the weekly average number of adult female Lepeophtheirus salmonis
+#' reported by the salmon companies.
+#'
+#' http://aquaculture.scotland.gov.uk/data/data.aspx
+#' https://scottishepa.maps.arcgis.com/apps/webappviewer/index.html?id=2218824350e5470e8026076d4138da58
 #'
 #' @param begin_ymd Beginning date in YYYY-MM-DD format
 #' @param end_ymd Ending date in YYYY-MM-DD format
@@ -15,19 +19,43 @@
 #' @export
 #'
 download_lice_counts <- function(begin_ymd, end_ymd, out_file) {
+  library(jsonlite); library(tidyverse); library(janitor)
+  webapp_yr <- year(today())
+  download_yrs <- unique(year(seq(ymd(begin_ymd), ymd(end_ymd), by=1)))
 
-  lice_url <- paste0("https://utility.arcgis.com/usrsvcs/servers/",
-                     "8999c2e86c7246cb93b420e810458293/rest/services/Secure/",
-                     "Sealice/MapServer/2/query?f=json&where=1%3D1&",
-                     "returnGeometry=false&spatialRel=esriSpatialRelIntersects&",
-                     "outFields=*&orderByFields=WEEK_BEGINNING%20DESC&outSR=4326&",
-                     "resultOffset=0&resultRecordCount=100000")
-  download.file(lice_url, "lice_counts.json")
-  data_df <- fromJSON("lice_counts.json")$features$attributes |>
-    clean_names(case="small_camel") |>
-    mutate(day=day(ymd(weekBeginning)),
-           month=month(weekBeginning)) |>
-    filter(between(date, ymd(begin_ymd), ymd(end_ymd)))
+  data_ls <- vector("list", 2)
+  if(webapp_yr %in% download_yrs) {
+    lice_url <- paste0("https://utility.arcgis.com/usrsvcs/servers/",
+                       "8999c2e86c7246cb93b420e810458293/rest/services/Secure/",
+                       "Sealice/MapServer/2/query?f=json&where=1%3D1&",
+                       "returnGeometry=false&spatialRel=esriSpatialRelIntersects&",
+                       "outFields=*&orderByFields=WEEK_BEGINNING%20DESC&outSR=4326&",
+                       "resultOffset=0&resultRecordCount=100000")
+    download.file(lice_url, "lice_counts.json")
+    data_ls[[1]] <- fromJSON("lice_counts.json")$features$attributes |>
+      clean_names(case="small_camel") |>
+      mutate(weekBeginning=ymd(weekBeginning),
+             day=day(weekBeginning),
+             month=month(weekBeginning)) |>
+      filter(between(weekBeginning, ymd(begin_ymd), ymd(end_ymd)))
+    file.remove("lice_counts.json")
+  }
+  if(any(download_yrs != webapp_yr)) {
+    download.file("https://map.sepa.org.uk/sealice/ms_sea_lice.zip", "lice.zip")
+    untar("lice.zip")
+    file.remove("lice.zip")
+    data_ls[[2]] <- read_csv("ms_sea_lice.csv", show_col_types=FALSE)|>
+      clean_names(case="small_camel") |>
+      mutate(weekBeginning=dmy(weekBeginning),
+             day=day(weekBeginning),
+             month=month(weekBeginning)) |>
+      filter(between(weekBeginning, ymd(begin_ymd), ymd(end_ymd))) |>
+      select(-easting, -northing, -nationalGridReference)
+    file.remove("ms_sea_lice.csv")
+  }
+
+  data_df <- data_ls |>
+    reduce(bind_rows)
   data_df |>
     write_csv(out_file)
   cat(nrow(data_df), "records saved to", out_file, "\n")
@@ -41,6 +69,8 @@ download_lice_counts <- function(begin_ymd, end_ymd, out_file) {
 #' Accessed through aquaculture.scotland.gov.uk. Downloads a csv of monthly fish
 #' biomass on each farm.
 #'
+#' http://aquaculture.scotland.gov.uk/data/data.aspx
+#'
 #' @param begin_ymd Beginning date in YYYY-MM-DD format
 #' @param end_ymd Ending date in YYYY-MM-DD format
 #' @param out_file Filename for the csv output
@@ -52,7 +82,7 @@ download_fish_biomass <- function(begin_ymd, end_ymd, out_file) {
   library(tidyverse); library(janitor)
   fish_url <- "http://aquaculture.scotland.gov.uk/csvexport/se_monthly_reports.csv"
   download.file(fish_url, out_file)
-  data_df <- read_csv(out_file) |>
+  data_df <- read_csv(out_file, show_col_types=FALSE) |>
     clean_names(case="small_camel") |>
     mutate(date=dmy(year)) |>
     filter(between(date, ymd(begin_ymd), ymd(end_ymd))) |>
@@ -62,3 +92,50 @@ download_fish_biomass <- function(begin_ymd, end_ymd, out_file) {
   cat(nrow(data_df), "records saved to", out_file, "\n")
 }
 
+
+
+
+
+#' Download Marine Scotland site details
+#'
+#' http://aquaculture.scotland.gov.uk/data/data.aspx
+#' http://aquaculture.scotland.gov.uk/data/site_details.aspx
+#'
+#' @param out_file Filename for the csv output
+#'
+#' @return Success message if csv is correctly saved
+#' @export
+download_ms_site_details <- function(out_file) {
+  library(tidyverse); library(janitor)
+  ms_url <- "http://aquaculture.scotland.gov.uk/csvexport/ms_site_details.csv"
+  download.file(ms_url, out_file)
+  data_df <- read_csv(out_file, show_col_types=FALSE) |>
+    clean_names(case="small_camel")
+  data_df |>
+    write_csv(out_file)
+  cat(nrow(data_df), "records saved to", out_file, "\n")
+}
+
+
+
+
+
+#' Download SEPA farm license details
+#'
+#' http://aquaculture.scotland.gov.uk/data/data.aspx
+#' http://aquaculture.scotland.gov.uk/data/lease_details.aspx
+#'
+#' @param out_file Filename for the csv output
+#'
+#' @return Success message if csv is correctly saved
+#' @export
+download_sepa_licenses <- function(out_file) {
+  library(tidyverse); library(janitor)
+  sepa_url <- "http://aquaculture.scotland.gov.uk/csvexport/se_licence_conditions.csv"
+  download.file(sepa_url, out_file)
+  data_df <- read_csv(out_file, show_col_types=FALSE) |>
+    clean_names(case="small_camel")
+  data_df |>
+    write_csv(out_file)
+  cat(nrow(data_df), "records saved to", out_file, "\n")
+}

099fc60b25fbbfc01ebf93c2970595de43f6ae3f
Sz-Tim <tim.szewczyk@gmail.com>
2025-01-16 14:54:23 +0000

    removed unnecessary lib copying

--- R/run_biotracker.R
+++ R/run_biotracker.R
@@ -1,7 +1,6 @@
 #' Run biotracker
 #'
 #' @param jdk_path Path to java jdk
-#' @param javaLib_path Path to commons-io-2.6.jar and netcdfAll-4.6.11.jar
 #' @param jar_path Path to biotracker.jar
 #' @param f_properties Properties file
 #' @param sim_dir Simulation directory
@@ -13,7 +12,6 @@
 #' @export
 #'
 run_biotracker <- function(jdk_path="/usr/local/java/jre1.8.0_211/bin/java",
-                           javaLib_path="jar/lib/",
                            jar_path="/home/sa04ts/biotracker/jar/biotracker.jar",
                            f_properties="sim_01.properties",
                            sim_dir="sim_01/",
@@ -27,7 +25,6 @@ run_biotracker <- function(jdk_path="/usr/local/java/jre1.8.0_211/bin/java",
   cat("---   Output dir:", sim_dir, "\n")
 
   proj_dir <- getwd()
-  file.copy(javaLib_path, sim_dir, recursive=T, overwrite=T)
   file.copy(f_properties, sim_dir)
   setwd(sim_dir)
 
@@ -47,6 +44,7 @@ run_biotracker <- function(jdk_path="/usr/local/java/jre1.8.0_211/bin/java",
     cat("--- Cleaning directory:", format(begin_clean, "%F %T"), "\n")
     clean_out_dir("arrivals_", "arrivals")
     clean_out_dir("connectivity_", "connectivity")
+    clean_out_dir("connectivityImmature_", "connectivityImmature")
     clean_out_dir("pstepsMature_", "pstepsMature")
     clean_out_dir("pstepsImmature_", "pstepsImmature")
     clean_out_dir("locations_", "locations")
